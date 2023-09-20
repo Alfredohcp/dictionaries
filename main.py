36f2fe6620d114a8550ffaf03ec0d2c0fd810817
Alfredohcp <125494329+Alfredohcp@users.noreply.github.com>
2023-09-20 17:54:46 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+from learn_dictionaries import *
+from learn_tuples import *
+from learn_sets import *
+from learn_booleans import *
+
+learn_dictionaries()
+learn_tuples()
+learn_sets()
+learn_booleans()
+
+
+###############################Proceed to last slide#################################
